8867b52ca4a9236e7735ac454a516a04b5be6df5
Pavel Sumin <pavsumin@gmail.com>
2026-03-31 11:33:12 +0700

    feat(admin): update logic for get content: you can just add your text to default content and supabase will update automaticly

--- lib/getContent.ts
+++ lib/getContent.ts
@@ -1,22 +1,21 @@
-import { defaultContent } from './defaultContent'
+import { ContentKey, defaultContent } from './defaultContent'
 import { supabase } from './supabase'
 
-type ContentItem = {
-	key: string
-	value: string
-}
-
-export async function getContent() {
+export async function getContent(): Promise<Record<ContentKey, string>> {
 	const { data } = await supabase.from('content').select('*')
 
 	const dbContent: Record<string, string> = {}
 
-	data?.forEach((item: ContentItem) => {
+	data?.forEach(item => {
 		dbContent[item.key] = item.value
 	})
 
-	return {
-		...defaultContent,
-		...dbContent,
+	const finalContent = {} as Record<ContentKey, string>
+
+	for (const key in defaultContent) {
+		finalContent[key as ContentKey] =
+			dbContent[key] ?? defaultContent[key as ContentKey]
 	}
+
+	return finalContent
 }
