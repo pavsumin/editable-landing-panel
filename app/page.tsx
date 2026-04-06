70215b657dbd864081e3acd81e88b182d4b93ff8
Pavel Sumin <pavsumin@gmail.com>
2026-04-06 10:44:39 +0700

    export const revalidate = 0

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from '@/components/pages/HomePage'
 import { getContent } from '@/lib/getContent'
 
+export const revalidate = 0
+
 export default async function Page() {
 	const content = await getContent()
 
